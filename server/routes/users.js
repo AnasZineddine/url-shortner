18136bdc1e6b1a78a11f7df0dd0472ecbff95c32
Anas Zineddine <a.zineddine2011@gmail.com>
2022-06-14 12:20:45 +0100

    WIP: Test database
    Change Mongodb image due to the fact that Prisma needs a replica set to
    connect to the database

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  await prisma.$connect();
+  const result = await prisma.link.create({
+    data: {
+      url: 'test',
+      shortUrl: 'test',
+    },
+  });
+  console.log({ result });
+  res.json(result);
 });
 
 module.exports = router;
