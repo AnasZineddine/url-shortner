0007fd4cb282a9deb6f4a4c837d67e9dbff4f98a
AnasZineddine <a.zineddine2011@gmail.com>
2022-06-26 22:25:36 +0100

    Add Wrap component for better UI

--- client/pages/shortned-urls.js
+++ client/pages/shortned-urls.js
@@ -7,15 +7,18 @@ import {
   Text,
   VStack,
   Button,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react';
 import useSWR from 'swr';
 import React from 'react';
 import Link from 'next/link';
+const moment = require('moment');
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 function Page({ index }) {
-  const { data, error } = useSWR(`http://localhost:5100/urls?page=${index}&limit=5`, fetcher);
+  const { data, error } = useSWR(`http://localhost:5100/urls?page=${index}&limit=15`, fetcher);
   const color = useColorModeValue('white', 'gray.900');
 
   if (error) return <div>failed to load</div>;
@@ -24,36 +27,47 @@ function Page({ index }) {
       <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
     );
   }
-  console.log({ data });
+  if (data.paging.total === 0) {
+    return (
+      <Flex align="center" justify="center">
+        No shortned URLs yet
+      </Flex>
+    );
+  }
+  console.log(data);
 
   return (
-    <>
+    <Wrap spacing="15px" justify="center" w="full" p={30}>
       {data.data.map((array) => (
-        <Box
-          // maxW={"445px"}
-          w="full"
-          bg={color}
-          boxShadow="2xl"
-          rounded="md"
-          p={6}
-          overflow="hidden"
-          key={array.shortUrl}
-        >
-          <VStack mt={6} spacing={4} align="center">
-            <Text fontWeight={600}>
-              <Link href={`http://localhost:3000/${array.shortUrl}`}>
-                <a target="_blank"> {`http://localhost:3000/${array.shortUrl} 🚀`} </a>
-              </Link>
-            </Text>
-            <Text fontWeight={100} noOfLines={[1, 2, 3]} istruncated="false" maxWidth="100px">
+        <WrapItem id={array.shortUrl}>
+          <Box
+            maxW="sm"
+            //w="100%"
+            bg={color}
+            boxShadow="2xl"
+            rounded="md"
+            p={6}
+            overflow="hidden"
+            key={array.shortUrl}
+          >
+            <VStack mt={6} spacing={4} align="center">
+              <Text id={array.shortUrl} color="gray.400">
+                {moment(array.createdAt).format('llll')}
+              </Text>
+              <Text fontWeight={600}>
+                <Link href={`http://localhost:3000/${array.shortUrl}`}>
+                  <a target="_blank"> {`http://localhost:3000/${array.shortUrl} 🚀`} </a>
+                </Link>
+              </Text>
+
               <Link href={array.url}>
-                <a target="_blank">{array.url} </a>
+                <a target="_blank">Original URL</a>
               </Link>
-            </Text>
-          </VStack>
-        </Box>
+            </VStack>
+          </Box>
+        </WrapItem>
       ))}
-    </>
+    </Wrap>
   );
 }
 
