bbe253e272c8b935fcfe1a9e643c2dc3f9649acb
Anas Zineddine <azineddi@e3r12p2.1337.ma>
2022-06-28 14:56:31 +0100

    Add infinite scroll
    Infinite scroll for better UX
    fixed same warnings and small ui changes

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,34 +1,6 @@
-/*import React from 'react';
-import { Flex, Stack } from '@chakra-ui/react';
-import { CopyIcon } from '@chakra-ui/icons';
-import Link from 'next/link';
-
- function Navbar() {
-  return (
-    <Flex backgroundColor="white" alignItems="center" justifyContent="space-between" pt={4} pb={4}>
-      <Stack spacing={4} isInline alignItems="center">
-        <CopyIcon />
-        <Link href="/">Home</Link>
-        <Link href="/shortned-urls">Shortned urls</Link>
-      </Stack>
-      <Flex>
-        <Stack spacing={4} isInline alignItems="center">
-          <Link mr={4} href="/">
-            English
-          </Link>
-          <Link href="/">OtherLanguage</Link>
-        </Stack>
-      </Flex>
-    </Flex>
-  );
-}
-
-export default Navbar; */
-
 import {
   Box,
   Flex,
-  Avatar,
   HStack,
   Link,
   IconButton,
@@ -40,6 +12,7 @@ import {
   useDisclosure,
   useColorModeValue,
   Stack,
+  Icon,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
@@ -70,47 +43,45 @@ export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-            <Box>Url-Shortner</Box>
-            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </HStack>
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <IconButton
+          size={'md'}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={'Open Menu'}
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems={'center'}>
+          <Box>Url-Shortner</Box>
+          <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+            {Links.map((link) => (
+              <NavLink key={link.name}>{link}</NavLink>
+            ))}
           </HStack>
-          <Flex alignItems={'center'}>
-            <Menu>
-              <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
-                <IconButton as={FcGlobe} />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>English</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
+        </HStack>
+        <Flex alignItems={'center'}>
+          <Menu>
+            <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
+              <Icon as={FcGlobe} w={10} h={10} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>English</MenuItem>
+              <MenuItem>Link 2</MenuItem>
+            </MenuList>
+          </Menu>
         </Flex>
+      </Flex>
 
-        {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </>
+      {isOpen ? (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as={'nav'} spacing={4}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
   );
 }
--- client/pages/shortned-urls.js
+++ client/pages/shortned-urls.js
@@ -10,86 +10,75 @@ import {
   Wrap,
   WrapItem,
 } from '@chakra-ui/react';
-import useSWR from 'swr';
 import React from 'react';
 import Link from 'next/link';
 const moment = require('moment');
+import useSWRInfinite from 'swr/infinite';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
-function Page({ index }) {
-  const { data, error } = useSWR(`http://localhost:5100/urls?page=${index}&limit=15`, fetcher);
+function Page() {
   const color = useColorModeValue('white', 'gray.900');
+  const grayColor = useColorModeValue('gray.50', 'gray.800');
 
+  const getKey = (pageIndex, previousPageData) => {
+    pageIndex = pageIndex + 1;
+    if (previousPageData && !previousPageData.data.length) return null;
+    return `http://localhost:5100/urls?page=${pageIndex}&limit=10`;
+  };
+
+  const { data: paginatedData, error, size, setSize } = useSWRInfinite(getKey, fetcher);
   if (error) return <div>failed to load</div>;
-  if (!data) {
+  if (!paginatedData) {
     return (
       <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
     );
   }
-  if (data.paging.total === 0) {
+  if (paginatedData[0].paging.total === 0) {
     return (
       <Flex align="center" justify="center">
         No shortned URLs yet
       </Flex>
     );
   }
-  console.log(data);
-
-  return (
-    <Wrap spacing="15px" justify="center" w="full" p={30}>
-      {data.data.map((array) => (
-        <WrapItem id={array.shortUrl}>
-          <Box
-            maxW="sm"
-            //w="100%"
-            bg={color}
-            boxShadow="2xl"
-            rounded="md"
-            p={6}
-            overflow="hidden"
-            key={array.shortUrl}
-          >
-            <VStack mt={6} spacing={4} align="center">
-              <Text id={array.shortUrl} color="gray.400">
-                {moment(array.createdAt).format('llll')}
-              </Text>
-              <Text fontWeight={600}>
-                <Link href={`http://localhost:3000/${array.shortUrl}`}>
-                  <a target="_blank"> {`http://localhost:3000/${array.shortUrl} 🚀`} </a>
-                </Link>
-              </Text>
-
-              <Link href={array.url}>
-                <a target="_blank">Original URL</a>
-              </Link>
-            </VStack>
-          </Box>
-        </WrapItem>
-      ))}
-    </Wrap>
-  );
-}
-
-function ShortnedUrls() {
-  const grayColor = useColorModeValue('gray.50', 'gray.800');
-  const [cnt, setCnt] = React.useState(2);
-  const pages = [];
-  for (let i = 1; i < cnt; i++) {
-    pages.push(<Page index={i} key={i} />);
-  }
 
   return (
     <Flex minH="100vh" align="flex-start" justify="center" bg={grayColor}>
       <Stack align="center">
-        {pages}
+        <InfiniteScroll
+          next={() => {
+            setSize(size + 1);
+          }}
+          dataLength={paginatedData.length}
+          hasMore
+        >
+          <Wrap spacing="15px" justify="center" w="full" p={30} bg={grayColor}>
+            {paginatedData.map((data) =>
+              data.data.map((array) => (
+                <WrapItem key={array.shortUrl}>
+                  <Box maxW="sm" bg={color} boxShadow="2xl" rounded="md" p={6} overflow="hidden">
+                    <VStack mt={6} spacing={4} align="center">
+                      <Text color="gray.400">{moment(array.createdAt).format('llll')}</Text>
+                      <Text fontWeight={600}>
+                        <Link href={`http://localhost:3000/${array.shortUrl}`}>
+                          <a target="_blank"> {`http://localhost:3000/${array.shortUrl} 🚀`} </a>
+                        </Link>
+                      </Text>
 
-        <Button onClick={() => setCnt(cnt + 1)} w="full" colorScheme="blue">
-          Load more
-        </Button>
+                      <Link href={array.url}>
+                        <a target="_blank">Original URL</a>
+                      </Link>
+                    </VStack>
+                  </Box>
+                </WrapItem>
+              )),
+            )}
+          </Wrap>
+        </InfiniteScroll>
       </Stack>
     </Flex>
   );
 }
 
-export default ShortnedUrls;
+export default Page;
